2d25c6488f4dd65c9f5f226ef33623d354c66253
Atharv Dewangan <ad298suncity@gmail.com>
2025-09-21 22:25:28 +0530

    fixed cors error

--- index.ts
+++ index.ts
@@ -10,15 +10,28 @@ import registrationRoutes from "./routes/registration.route.js";
 const app = express();
 connectDB();
 
-app
-  .use(express.json())
-  .use(cookieParser())
-  .use(
-    cors({
-      origin: ["http://localhost:3000", "https://ziva-alpha.vercel.app/"],
-      credentials: true,
-    })
-  );
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://ziva-alpha.vercel.app",
+  "https://eventhub.athard.in",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+app.use(express.json()).use(cookieParser());
 app
   .use("/api/auth", authRoutes)
   .use("/api", registrationRoutes)
