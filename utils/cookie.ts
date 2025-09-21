1f9f5e004734ab3d3b737c78e907bf45b0a74f59
Atharv Dewangan <ad298suncity@gmail.com>
2025-09-21 23:11:21 +0530

    cookie changes

--- utils/cookie.ts
+++ utils/cookie.ts
@@ -2,11 +2,14 @@ import type { Response } from "express";
 
 const COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production", 
-  sameSite: process.env.NODE_ENV === "production" ? "none" as const : "lax" as const,
-  maxAge: 7 * 24 * 60 * 60 * 1000, 
+  // secure: process.env.NODE_ENV === "production",
+  secure: true,
+  sameSite:
+    process.env.NODE_ENV === "production"
+      ? ("none" as const)
+      : ("lax" as const),
+  maxAge: 7 * 24 * 60 * 60 * 1000,
 };
-
 
 export function setTokenCookie(res: Response, token: string) {
   res.cookie("token", token, COOKIE_OPTIONS);
